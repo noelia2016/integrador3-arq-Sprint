8faf3386b71d39e32e29d3e712608807d9cecb07
Diego Rodriguez <dsrrdiego@hotmail.com>
2023-09-27 11:52:08 -0300

    llegando al get

--- sprint-t3/src/main/java/controlers/PersonaC.java
+++ sprint-t3/src/main/java/controlers/PersonaC.java
@@ -5,6 +5,7 @@ package controlers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import modelos.Persona;
 import repos.PersonaRepo;
 
 @RestController
-@RequestMapping("/p")
+// @RequestMapping("/p")
 public class PersonaC {
     @Qualifier("PerRep")
     @Autowired
@@ -22,8 +23,13 @@ public class PersonaC {
         this.personaRepo=personaRepo;
     }
 
-    @GetMapping("/")
+    @GetMapping("/persona")
     public Iterable<Persona> persona() {return personaRepo.findAll();}
 
+    @PostMapping("/cargar")
+    public void cargar(){
+        Persona p=new Persona((long)3,"tres");
+        personaRepo.save(p);
+    }
     
 }
